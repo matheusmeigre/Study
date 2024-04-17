72cf97c1c122b17451dfbd25381bc33340c5ca41
matheusmeigre <142261154+matheusmeigre@users.noreply.github.com>
2024-04-16 21:02:28 -0300

    manipulating strings with py

---
+++ study-python/strings_com_python.py
@@ -0,0 +1,16 @@
+nome = "mATHeuS"
+
+# maiuscula, minuscula e titulo
+print(nome.upper())
+print(nome.lower())
+print(nome.title())
+
+# eliminando espacos em branco
+print(nome.strip())
+print(nome.rstrip())
+print(nome.lstrip())
+
+# juncoes e centralizacao
+print(".".join(nome))
+print(nome.center(10,"#"))
+
